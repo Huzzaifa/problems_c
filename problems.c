2e11bef63a7c56fe10d20029073fcc82844f174c
Huzzaifa <48279541+Huzzaifa@users.noreply.github.com>
2019-03-19 22:38:05 +0100

    adding problems

---
+++ problems.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //Area of rectangle
+  /*float width = get_float("Width: ");
+  float Length = get_float("length: ");
+  float Area = width * Length;
+  printf("Area: %f\n",Area);*/
+
+  //Marks in percentage
+  /*int total_marks = get_int("total marks: ");
+  float obtained_marks = get_float("obtained marks: ");
+  float percentage = (obtained_marks / total_marks)*100;
+  printf("Percentage:  %f\n",percentage);*/
+
+  //sales tax
+  /*float amount = get_float("Amount: ");
+  float tax_percent = get_float("tax percent: ");
+  float sales_tax = (amount * tax_percent)/100;
+  printf("sales tax: %f\n",sales_tax);*/
+
+  // no of minutes from hours
+ /* float time_hrs = get_float("time in hours: ");
+  float time_min = time_hrs * 60;
+  float time_sec = time_min * 60;
+  printf("time_min: %f\ntime_sec: %f\n",time_min,time_sec);*/
+
+  // Centripetal Force
+  /*float mass_Kg = get_float("Mass in kg: ");;
+  float mass_g = 1000 * mass_Kg;
+  float velocity_ms = get_float("velocity in ms: ");
+  float radius = get_float("radius in m: ");
+  float Centripetal_force = (mass_g * (velocity_ms * velocity_ms)) /radius;
+  printf("Centripetal Force: %f\n",Centripetal_force);*/
+
+}
