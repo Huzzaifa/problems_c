e860ac75161325938d3a99425b93091ad465b2bc
Huzzaifa <48279541+Huzzaifa@users.noreply.github.com>
2019-03-20 21:07:15 +0100

    Adding problems11-15

---
+++ problems11-15.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+// A=?
+    /*float x = get_float("x: ");
+    float y = get_float("y: ");
+    float z = get_float("z: ");
+    if(z == 0)
+    {
+        printf("A: Undefine \n");
+    }
+    else
+    {
+         float A =(4 * x - 3 * y) / 2 * z;
+
+          printf("A: %f\n",A);
+    }*/
+
+ // D=?
+   /* float x = get_float("x: ");
+    float v = get_float("v: ");
+    float t_hrs = get_float("t_hrs: ");
+    float t_s = 3600*t_hrs;
+    float a = get_float("a: ");
+    float D = x + v * t_s + 0.5*(a*t_s*t_s);
+    printf("D: %f\n",D);*/
+
+ // A= ?
+    /*float a = get_float("a: ");
+    float b = get_float("b: ");
+    float c = get_float("c: ");
+     if(c == 0)
+    {
+        printf("A: Undefine \n");
+    }
+    else
+    {
+    float A = (a - b)*(a - b)/ 2*c;
+
+    printf("A: %f\n",A);
+    }*/
+
+ //E = ?
+    /*float m = get_float("mass in gram: ");
+    float v = get_float("v: ");
+    float g = get_float("g: ");
+    float h = get_float("height in meters: ");
+    float E = ((m*v*v)/2) + m*g*h;
+    printf("E: %f/n",E);*/
+
+//A= ?
+        /*float a = get_float("a: ");
+        float b = get_float("b: ");
+        float A = ((a + b)*(a + b)) - 2*a*b;
+        printf("A: %f\n",A);*/
+
+
+
+}
