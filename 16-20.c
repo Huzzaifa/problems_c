a7e148905dfb2161c2c6b903ca47948dc9c7f544
Huzzaifa <48279541+Huzzaifa@users.noreply.github.com>
2019-03-20 21:48:33 +0100

    addin problem 16-20

---
+++ 16-20.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+// Find absolute value of difference of two numbers
+    /*int a = get_int("a: ");
+    int b = get_int("b: ");
+    int abs_diff = abs(a - b);
+    printf("Absolute Difference: %i\n",abs_diff);*/
+
+// Find smaller  out of 2 numbers
+
+   /*int a = get_int("A ");
+   int b = get_int("B: ");
+
+   if(a < b)
+   {
+       printf("A is smaller: ");
+   }
+   else if(b < a)
+   {
+       printf("B is smaller: ");
+   }
+ else
+  {
+     printf("A and B are Equal: ");
+  }*/
+
+
+// Find number is negative or not
+
+  /*int a = get_int("a: ");
+  if(a < 0)
+  {
+      printf("Number is negative: ");
+
+  }
+  else if(a > 0)
+  printf("Number is positive: ");
+  else
+  printf("Number is 0: ");*/
+
+
+ // Find 2 numbers  are equal or not
+
+   /*int a = get_int("A: ");
+   int b = get_int("B: ");
+
+   if(a == b)
+   {
+       printf("A and B are equal: ");
+   }
+   else
+       printf("Unequal: ");*/
+
+ // Multiply two numbers if there diffrence is greater than zero
+
+   /*int a = get_int("A: ");
+   int b = get_int("B: ");
+   int difference = a - b;
+
+   if(difference > 0)
+   {
+       int c = a * b;
+       printf("Answer: %i\n",c);
+   }
+   else if(difference < 0)
+   printf("B is greater than A: ");
+   else
+   printf("A and B are equal: ");*/
+
+
+
+}
