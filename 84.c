9b55b2bb9cb8a0154394af1d3eaf46d6b8562613
Huzzaifa <48279541+Huzzaifa@users.noreply.github.com>
2019-03-22 17:45:20 +0100

    addin 84

---
+++ 84.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+
+// Find Sum of ith powers of first n integers, where i can be any number.
+
+    /*int n = get_int("N: ");
+    int l = get_int("Power: ");
+    int sum = 0;
+    int sq[n];
+    sq[0] = 0;
+    for (int i = 0; i < n ; i++ )
+    {
+        sum = sum + sq[i];
+        for(int k = 1; k < n; k++ )
+        {
+
+            int z = k;
+            int y = k;
+
+            for(int j = 1; j < l ; j++)
+            {
+                y = y * z;
+                if (j == l-1)
+                {
+                    sq[k] = y;
+                }
+            }
+
+        }
+        printf("%i\n",sq[i]);
+
+    }
+    printf("Sum: %i\n",sum);
+}*/
