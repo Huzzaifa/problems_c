679353d5f51f92648fa7d9a6580a907365b65e94
Huzzaifa <48279541+Huzzaifa@users.noreply.github.com>
2019-03-22 16:16:12 +0100

    adding 52-56

---
+++ 52-56.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+
+// 52. Find/Display first n integers.
+
+  /*int n = get_int ("N: ");
+  for (int i = 0; i < n; i++)
+  {
+      printf("%i\n",i);
+  }*/
+
+
+// 53. Find sum of first n integers
+
+  /*int n = get_int ("N: ");
+  int sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+      sum = sum + i;
+
+  }
+  printf("Sum: %i\n",sum);*/
+
+
+// 54. Find factorial of a number
+
+   /*int number = get_int("Number: ");
+
+   int factorial = 1;
+
+   for(int i = number; i > 0; i--)
+   {
+       factorial = factorial * i;
+   }
+   printf("Factorial: %i\n",factorial);*/
+
+// 55. Find X^y?
+
+   /*int x = get_int("X: ");
+   int y = get_int("y: ");
+    int m = x;
+   for ( int i = 1; i < y; i++)
+   {
+
+      x = x * m;
+   }
+   printf("Answer: %i\n",x);*/
+
+// 56. Find sum of first n even numbers.
+
+  /*int n = get_int("N: ");
+  int sum = 0;
+
+  for (int i = 0; i < n; i = i + 2)
+  {
+      sum = sum + i;
+
+  }
+  printf("Sum: %i\n",sum);*/
+}
